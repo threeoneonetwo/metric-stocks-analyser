5b1af3471ab14395f966677638725e8aa275349d
Vansh Pandita <vpandita@iu.edu>
2026-05-26 18:08:34 +0530

    Make logo visibly heavier

--- components/site-chrome.tsx
+++ components/site-chrome.tsx
@@ -12,8 +12,12 @@ export function TopBar({ reportActions = false, ticker }: TopBarProps) {
       <div className="mx-auto flex h-12 max-w-[42rem] items-center justify-between">
         <Link
           href="/"
-          className="text-4xl font-extrabold leading-none tracking-[-0.04em] text-black"
-          style={{ fontWeight: 1000 }}
+          className="text-4xl font-extrabold leading-none tracking-[-0.06em] text-black"
+          style={{
+            fontWeight: 1000,
+            textShadow:
+              "0.45px 0 0 currentColor, -0.45px 0 0 currentColor, 0 0.45px 0 currentColor",
+          }}
         >
           metric
         </Link>
